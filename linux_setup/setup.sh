5630b6a97cca0a9d9e49c67edb81e8f0a64e6e69
Sören <35612644+sjvie@users.noreply.github.com>
2025-05-23 12:25:27 +0200

    Update setup.sh

--- linux_setup/setup.sh
+++ linux_setup/setup.sh
@@ -6,3 +6,6 @@ curl -fsSL https://raw.githubusercontent.com/sjvie/Util/refs/heads/main/linux_se
 echo ""
 echo "--- Setting up ~/.inputrc ---"
 curl -fsSL https://raw.githubusercontent.com/sjvie/Util/refs/heads/main/linux_setup/setup_inputrc.sh | bash
+
+echo ""
+echo "Re-log or re-source the modified files for the changes to take effect"
